0e179d566564d7fd345ede3a578b7e4b1b5255d4
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-18 14:26:36 -0400

    Completed challenge in java displaying occupation.

---
+++ JavaChallenges/Intro Challenge 1.java
@@ -0,0 +1,10 @@
+// Creation of a new Java project with "Employee.java"
+// display name and what they do in two lines.
+
+solution:
+class Tester {
+	public static void main(String[] args) {
+		System.out.println("My name is Matthew Robinson.");
+		System.out.println("My occupation is Systems Engineer.");
+	}
+}
