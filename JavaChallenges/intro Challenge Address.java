e394501393685f07b4ed78c7c52d7844cdc08006
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-18 15:14:39 -0400

    Completed challenge printing out address.

---
+++ JavaChallenges/intro Challenge Address.java
@@ -0,0 +1,13 @@
+// Create a new Java project with new address file and display that address.
+
+
+solution:
+class Tester {
+	public static void main(String[] args) {
+	    System.out.println("Door No: A29");
+		System.out.println("Street: St. FlatDrive");
+		System.out.println("City: Rockford");
+		System.out.println("ZIP Code: 62746");
+
+	}
+}
