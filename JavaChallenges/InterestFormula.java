36e862990d93c09c86a767d391511b9b9b5dec2f
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-18 17:31:47 -0400

    Set up initial coding challenge for gernerating interest in java.

---
+++ JavaChallenges/InterestFormula.java
@@ -0,0 +1,6 @@
+// Build a program that calculates the Interest by using the formula for interest and return the interest.
+//
+//  Interest Formula = (principal*rate of interest*time)/100
+
+
+solution:
