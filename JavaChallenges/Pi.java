268683ec61fb55a8495342e5ffe41bc965f53ba5
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-19 11:29:14 -0400

    Implemented solution using the Math method to mathmatically perform the task for find the area of a circle.

---
+++ JavaChallenges/Pi.java
@@ -0,0 +1,16 @@
+// Build a program to finding the area of a circle and display the calculated area.
+//
+// Area = pi*radius*radius
+//
+// The value of pi is 3.14.
+
+
+solution:
+class Tester {
+	public static void main(String[] args) {
+	    int r = 4;
+	    double area;
+	    area = (r*r)*Math.PI;
+	    System.out.println("Area of the circle is :"+area);
+	}
+}
