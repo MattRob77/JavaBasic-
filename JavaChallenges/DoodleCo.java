b11a9b7873f074c9c9a8424b20740741757500bd
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-19 11:49:34 -0400

    Added else statement with pick up charge.

--- JavaChallenges/DoodleCo.java
+++ JavaChallenges/DoodleCo.java
@@ -13,3 +13,28 @@ public class Customer {
 		System.out.println("Total cost: " + totalCost);
 	}
 }
+
+
+
+
+
+
+public class Customer {
+	public static void main(String[] args) {
+		String customerType = "Regular";
+		int quantity = 1;
+		int unitPrice = 75;
+		int totalCost = 0;
+		int discount = 10;
+		int pickUpCharge = 5;//added PickUp charge
+		totalCost = unitPrice * quantity;
+		if (customerType == "Regular") {
+			totalCost = totalCost - (totalCost * discount / 100);
+			System.out.println("You are a regular customer and eligible for 10% discount");
+		} else {
+			totalCost = totalCost + pickUpCharge;
+			System.out.println("You need to pay an additional pick up charge of $5");
+		}
+		System.out.println("Total cost: " + totalCost);
+	}
+}
