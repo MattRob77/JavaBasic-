f62fad4abc11d7edb3d7bd99153dcd098b83b059
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-20 09:16:32 -0400

    Refractured solution.

--- JavaChallenges/selectionControl1.java
+++ JavaChallenges/selectionControl1.java
@@ -17,3 +17,20 @@ class Tester {
 		}
 	}
 }
+
+
+
+
+class Tester {
+	public static void main(String[] args) {
+		int num1 = 6;
+		int num2 = 5;
+
+		if (num1 == num2) {
+		    System.out.println(num1 + num2);
+		}
+		else {
+		    System.out.println((num1 + num2)*2);
+		}
+	}
+}
