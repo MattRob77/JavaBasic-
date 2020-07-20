d43fe9dc1877ed2b0f8e7057af2f82383467816e
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-19 22:03:36 -0400

    Implemented solution using simple math to compute the sum and doubling.

---
+++ JavaChallenges/selectionControl1.java
@@ -0,0 +1,19 @@
+// Build a program to display the sum of two given numbers if the numbers are same. If the numbers are not same, display the double of the sum.
+
+
+
+solution:
+class Tester {
+	public static void main(String[] args) {
+		int Num1 = 6;
+		int Num2 = 5;
+		int Num3 = 5;
+
+		if(Num1 <= Num2 && Num1 >= Num3);{
+		System.out.println(6 + 5 *3 +1);
+		}
+		else if(Num1 <= Num2 && Num1 <= Num3); {
+		System.out.println(5 + 5);
+		}
+	}
+}
