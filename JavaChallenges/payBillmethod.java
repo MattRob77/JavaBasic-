20803f3f22202d286f46e8c9eee796e097258353
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-21 16:43:59 -0400

    Added another implementation.

--- JavaChallenges/payBillmethod.java
+++ JavaChallenges/payBillmethod.java
@@ -5,3 +5,13 @@ public void payBill(double totalPrice, double discountPercentage) {
 			+ ", your final bill amount after discount is: "
 			+ (int) (priceAfterDiscount * 100) / 100.0);
 }
+
+
+
+
+public double payBill(double totalPrice) {
+	double discountPercentage = 10;
+	System.out.println("Calculating final amount to be paid.....");
+	double priceAfterDiscount = totalPrice * (1 - (discountPercentage / 100));
+	return priceAfterDiscount;
+}
