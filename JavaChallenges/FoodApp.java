f7e7dc9b62c1acb3626c0522abc63726c4c8afcc
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-22 17:25:17 -0400

    Updated Foodapp with implemented constructor.

--- JavaChallenges/FoodApp.java
+++ JavaChallenges/FoodApp.java
@@ -5,6 +5,20 @@ public class Restaurant {
 	public String restaurantAddress;
 	public float rating;
 
+	public Restaurant() {
+		System.out.println("Parameterless constructor called");
+	}
+
+	public Restaurant(String restaurantName, long restaurantContact, String restaurantAddress, float rating ) {
+
+
+		this.restaurantName = restaurantName;
+		this.rating = rating;
+		this.restaurantContact = restaurantContact;
+		this.restaurantAddress = restaurantAddress;
+
+	}
+
 	public void displayRestaurantDetails() {
 		System.out.println("Restaurant Details \n************");
 		System.out.println("Restaurant Name : " + restaurantName);
@@ -14,19 +28,17 @@ public class Restaurant {
 		System.out.println();
 
 	}
+
 }
 
 class Tester {
 	public static void main(String[] args) {
 
 		Restaurant restaurant = new Restaurant();
-
 		restaurant.restaurantName = "Peace Food";
 		restaurant.rating = 4;
 		restaurant.restaurantContact = 998867676;
 		restaurant.restaurantAddress = "BH1019, Vine Street, Loinfield";
-
-
 		restaurant.displayRestaurantDetails();
 	}
 }
