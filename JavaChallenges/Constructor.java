99ed82671a801711c93c306c883403f862d4599a
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-22 16:33:43 -0400

    Added this keyword

--- JavaChallenges/Constructor.java
+++ JavaChallenges/Constructor.java
@@ -30,3 +30,19 @@ class Tester{
 
     }
 }
+
+
+
+public class Customer {
+	public String customerId;
+	public String customerName;
+	public long contactNumber;
+	public String address;
+	public Customer(String customerId, String customerName, long contactNumber,
+			String address) {
+		this.customerId = customerId;
+		this.customerName = customerName;
+		this.contactNumber = contactNumber;
+		this.address = address;
+	}
+}
