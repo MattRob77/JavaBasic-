29bf8471fdc3e1b96002a46ff5d5c781825cc06b
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-25 15:44:19 -0400

    Created notes about accessing elements in an array.

--- JavaChallenges/ArrayEx.java
+++ JavaChallenges/ArrayEx.java
@@ -28,3 +28,9 @@ class Restaurant {
 		System.out.println();
 	}
 }
+//Accessing the daya in the array
+long[] restaurantContacts = new long[3];
+restaurantContacts [0] = 9992346725L; // Elements can be updated and accessed with the help of index
+restaurantContacts [1] = 9992346726L;
+restaurantContacts [2] = 9992346727L;
+System.out.println(restaurantContacts [1]) ;// Accessing and displaying the element at the 1st index
